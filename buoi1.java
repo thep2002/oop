db130f862d92b549b19bc21e13a53d9f5540d352
thep2002 <59258172+thep2002@users.noreply.github.com>
2022-10-25 16:28:22 +0700

    fistcomit

---
+++ buoi1.java
@@ -0,0 +1,10 @@
+package labjava;
+
+public class buoi1 {
+
+	public static void main(String[] args) {
+		System.out.println("Xin chao \n cac ban");
+		System.out.println("Hello \t world!");
+	}
+
+}
